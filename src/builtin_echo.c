ed127d3ffc248a1e9116cb93ab0f3eba39de80d4
andoitzcp <andoitzcp@gmail.com>
2024-11-28 20:55:54 +0100

    create echo built-in

--- src/builtin_echo.c
+++ src/builtin_echo.c
@@ -2,12 +2,19 @@
 
 void ft_echo(t_ast_node *node)
 {
+    char flag;
     char **args;
 
-    args = node->args;
+    flag = 0;
+    args = node->args + 1;
+    if ((flag = ft_strncmp("-n", *args, 3)) == 0)
+        args++;
+    printf("%s", *args);
+    args++;
     while (args)
     {
-
+        printf(" %s", *args);
     }
-
+    if (flag)
+        printf("\n");
 }
