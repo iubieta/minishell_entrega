770e6b90afde9b332922338a7ee472f4fd69ff3d
andoitzcp <andoitzcp@gmail.com>
2025-01-02 18:45:05 +0100

    builtin echo tested and funcitioning

--- src/builtin_echo.c
+++ src/builtin_echo.c
@@ -1,20 +1,70 @@
 #include "minishell.h"
 
-void ft_echo(t_ast_node *node)
+void ft_echo(t_tree *node)
 {
-    char flag;
+    int i;
+    int flag;
     char **args;
 
     flag = 0;
-    args = node->args + 1;
-    if ((flag = ft_strncmp("-n", *args, 3)) == 0)
-        args++;
-    printf("%s", *args);
-    args++;
-    while (args)
+    args = node->args;
+    if (args[1] == NULL)
     {
-        printf(" %s", *args);
+        printf("\n");
+        return;
     }
-    if (flag)
+    i = 0;
+    if (strncmp(args[++i], "-n", 2) == 0)
+        flag = 1;
+    i+= flag;
+    if (args[i])
+        printf("%s", args[i]);
+    else
+        return;
+    while (args[++i])
+        printf(" %s", args[i]);
+    if (!flag)
         printf("\n");
 }
+
+/* int main (void) */
+/* { */
+/*     char *s; */
+/*     t_tree node; */
+
+/*     s = strdup("echo -n hola mundo"); */
+/*     node.args = ft_split(s, ' '); */
+/*     printf("===TEST 1===\n"); */
+/*     printf("EXPECTED: hola mundo\n"); */
+/*     printf("ACTUAL: "); */
+/*     ft_echo(&node); */
+/*     printf("\n"); */
+/*     free(s); */
+
+/*     s = strdup("echo hola mundo"); */
+/*     node.args = ft_split(s, ' '); */
+/*     printf("===TEST 2===\n"); */
+/*     printf("EXPECTED: hola mundo\n\n"); */
+/*     printf("ACTUAL: "); */
+/*     ft_echo(&node); */
+/*     printf("\n"); */
+/*     free(s); */
+
+/*     s = strdup("echo"); */
+/*     node.args = ft_split(s, ' '); */
+/*     printf("===TEST 3===\n"); */
+/*     printf("EXPECTED:\n\n"); */
+/*     printf("ACTUAL:"); */
+/*     ft_echo(&node); */
+/*     printf("\n"); */
+/*     free(s); */
+
+/*     s = strdup("echo -n"); */
+/*     node.args = ft_split(s, ' '); */
+/*     printf("===TEST 4===\n"); */
+/*     printf("EXPECTED:\n"); */
+/*     printf("ACTUAL:"); */
+/*     ft_echo(&node); */
+/*     printf("\n"); */
+/*     free(s); */
+/* } */
